66af68497d214cb11f281028bb9d775b9fbbd05f
root <rbalajis25@gmail.com>
2017-04-12 02:12:22 +0530

    revision 4

--- project5/final_project/poi_id.py
+++ project5/final_project/poi_id.py
@@ -1,8 +1,6 @@
-
 import sys
 import pickle
 sys.path.append("../tools/")
-
 from feature_format import featureFormat, targetFeatureSplit
 from tester import dump_classifier_and_data
 from sklearn.feature_selection import SelectKBest
@@ -10,12 +8,8 @@ from sklearn.feature_selection import chi2
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
-### Task 1: Select what features you'll use.
-### features_list is a list of strings, each of which is a feature name.
-### The first feature must be "poi".
-features_list = ['poi','salary']
-# You will need to use more features
-### Load the dictionary containing the dataset
+features_list = ['poi']
+# read data
 with open("final_project_dataset.pkl", "r") as data_file:
     data_dict = pickle.load(data_file)
 
@@ -67,25 +61,18 @@ for name in my_dataset:
         my_dataset[name]['from_fraction'] = 0
         my_dataset[name]['to_fraction'] = 0
 
-total_feature.insert(0,'from_fraction')
-total_feature.insert(0,'to_fraction')
-my_features = total_feature
+for i in range(0,len(total_feature)):
+    features_list.append(total_feature[i])
+features_list.append('from_fraction')
+features_list.append('to_fraction')
+#total_feature.insert(0,'poi')
+my_features = features_list
 data = featureFormat(my_dataset, my_features, sort_keys = True)
 
 labels, features = targetFeatureSplit(data)
 
-my_features = total_feature
-k_best = SelectKBest(k=7)
-k_best.fit(features, labels)
-results_list = zip(k_best.get_support(), my_features[1:],k_best.scores_)
-best_features = []
-for i in range(0,len(results_list)):
-    if results_list[i][0] == True:
-        best_features.insert(0,results_list[i][1])
-print results_list
 
-data = featureFormat(my_dataset, best_features, sort_keys = True)
-labels, features = targetFeatureSplit(data)
+
 
 from sklearn.linear_model import Perceptron
 from sklearn.naive_bayes import GaussianNB
@@ -94,6 +81,7 @@ from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_sc
 from sklearn.cross_validation import train_test_split
 from sklearn.grid_search import GridSearchCV
 from sklearn import svm
+from sklearn import preprocessing
 def evaluate_clf(clf,params,feature,lable):
     X_train, X_test, y_train, y_test = train_test_split(feature, lable)
     clf= GridSearchCV(clf, params)
@@ -108,18 +96,66 @@ def evaluate_clf(clf,params,feature,lable):
     print 'recall score {}'.format(recall_score(y_test,y_pred,average = 'micro'))
     #print mean(recall)
     #print mean(precision)
-best_features.insert(0,'poi')
+#best_features.insert(0,'poi')
+#my_features.insert(0,'poi')
 #clf.fit()
-lsvc=svm.LinearSVC(penalty='l2')
 # C parameter is used for how much classification curve has to make
 #evaluate_clf(lsvc,[{'C':[1,10,100]}],features,labels)
-from sklearn.ensemble import RandomForestClassifier
-rforest = RandomForestClassifier(n_estimators = 3,min_samples_leaf = 3)
-logreg = LogisticRegression(tol = 0.001, C = 10**-8, penalty = 'l2', random_state = 42)
-# min samples leaf is used to determine how much sample minimum requrired to go further split
-clf = GaussianNB()
-evaluate_clf(rforest,[{'min_samples_leaf':[3,5,10,13,15],'min_samples_split':[.1,0.2,.5]}],features,labels)
-rforest = RandomForestClassifier(n_estimators = 3,min_samples_leaf = 15, min_samples_split = .2)
-print best_features
+#def best_k_selector():
 
-dump_classifier_and_data(rforest, my_dataset, best_features)
+
+from sklearn.ensemble import RandomForestClassifier
+
+for best_k in range(4,10):
+    k_best = SelectKBest(k=best_k)
+    data = featureFormat(my_dataset, my_features, sort_keys = True)
+    labels, features = targetFeatureSplit(data)
+    k_best.fit(features, labels)
+    results_list = zip(k_best.get_support(), my_features[1:],k_best.scores_)
+    best_features = []
+    for i in range(0,len(results_list)):
+        if results_list[i][0] == True:
+            best_features.insert(0,results_list[i][1])
+    best_features.insert(0,'poi')
+    data = featureFormat(my_dataset, best_features, sort_keys = True)
+    labels, features = targetFeatureSplit(data)
+    print 'k values {}'.format(best_k)
+    clf = GaussianNB()
+    X_train, X_test, y_train, y_test = train_test_split(features, labels,random_state=5)
+    clf.fit(X_train, y_train)
+    print "best estimator"
+    y_pred = clf.predict(X_test)
+    #print classification_report(y_test,y_pred)
+    print 'precision score {}'.format(precision_score(y_test,y_pred,average = 'micro'))
+    print 'recall score {}'.format(recall_score(y_test,y_pred,average = 'micro'))
+
+# we are testing value on diffrent k value
+# where k is 7 give us a great prediction
+k_best = SelectKBest(k=6)
+data = featureFormat(my_dataset, my_features, sort_keys = True)
+labels, features = targetFeatureSplit(data)
+k_best.fit(features, labels)
+results_list = zip(k_best.get_support(), my_features[1:],k_best.scores_)
+best_features = []
+for i in range(0,len(results_list)):
+    if results_list[i][0] == True:
+        best_features.insert(0,results_list[i][1])
+best_features.insert(0,'poi')
+data = featureFormat(my_dataset, best_features, sort_keys = True)
+labels, features = targetFeatureSplit(data)
+rforest = RandomForestClassifier(n_estimators=4)
+print 'random forest'
+evaluate_clf(rforest,[{'min_samples_split':[5,10,15,20],'min_samples_leaf':[.2,.3,.4,.5]}],features,labels)
+print 'GaussianNB'
+clf = GaussianNB()
+X_train, X_test, y_train, y_test = train_test_split(preprocessing.scale(features), labels,random_state=5)
+clf.fit(X_train, y_train)
+print "best estimator"
+y_pred = clf.predict(X_test)
+#print classification_report(y_test,y_pred)
+print 'precision score {}'.format(precision_score(y_test,y_pred,average = 'micro'))
+print 'recall score {}'.format(recall_score(y_test,y_pred,average = 'micro'))
+'''
+we got good score at RandomForestClassifier
+'''
+dump_classifier_and_data(clf, my_dataset, best_features)
